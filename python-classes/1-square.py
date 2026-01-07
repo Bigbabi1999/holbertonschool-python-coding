1115e7b8a0b5aee5204199c3f0761bc8405fe179
Ali Moos <mooosali446@gmail.com>
2026-01-07 06:37:26 +0000

    m

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,8 +2,8 @@
 """Defines a Square class with size validatiomn"""
 
 class Square:
-    
-     def__init__(self, size=0):
+
+    def__init__(self, size=0):
         """Represents a square"""
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
@@ -11,3 +11,4 @@ class Square:
         raise ValueError("size must be >= 0")
 
     self.__size = size
+
