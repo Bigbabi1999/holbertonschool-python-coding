091ff2b0d535f868d0fe3fe6884d051ceffde77c
Ali Moos <moosali446@gmail.com>
2026-01-12 10:55:30 +0000

    m

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
         Args:
            size (int): size of the square
         """
-        if not isinstance(size, int):
+        if not def area(self):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
