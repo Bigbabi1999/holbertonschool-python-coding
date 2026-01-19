71238bc9ab277668c2e7511be81f6e65bb35710b
Ali Moos <moosali446@gmail.com>
2026-01-19 10:16:39 +0400

    m

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Represents a square"""
 
-    def _init_(self, size=0):
+    def __init__(self, size=0):
         """Initializes a Square instance"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -14,6 +14,7 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = size
+        
 
     def area(self):
         """Returns the area of the square"""
