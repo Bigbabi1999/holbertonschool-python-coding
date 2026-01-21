15300b51d6a0fad4f7c90e9abe53fddf6a18940e
Ali Moos <moosali446@gmail.com>
2026-01-21 18:21:52 +0400

    m

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -16,7 +16,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """Set the size with validation""" 
+        """Set the size with validation"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
 
