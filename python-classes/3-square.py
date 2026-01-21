79a5fcff2fae9aa0fff1754776ef35c5650b52be
Ali Moos <moosali446@gmail.com>
2026-01-21 18:19:31 +0400

    m

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -5,12 +5,22 @@
 class Square:
     """Represents a square"""
 
-    def __init__(self, value=0):
+    def __init__(self, size=0):
         """Initializes a Square instance"""
-        if not isinstance(self, value):
+        self.size = size
+
+    @property
+    def size(self):
+       """Retrieve the size"""
+       return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size with validation""" 
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
 
-        if self < value:
+        if value < 0:
             raise ValueError("size must be >= 0")
 
         self.__size = value
