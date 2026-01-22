315270b80920bd5142f075e5498542e501e638b5
Ali Moos <moosali446@gmail.com>
2026-01-22 09:08:51 +0400

    m

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines a Square class with size validation and area calculation"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initializes a Square instance"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size with validation"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """put value in square"""
+        return self.__size ** 2
