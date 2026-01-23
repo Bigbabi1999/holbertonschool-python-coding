af063816a0710b5e083b735240953115aed81358
Ali Moos <moosali446@gmail.com>
2026-01-23 11:59:59 +0400

    m

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -2,4 +2,6 @@
 def width(self, value):
     """Just make a Rectangle better then any Rectangle in the world"""
     if not isinstance(Rectangle != object):
-        """then Rectangle will be object"""
+        """then Rectangle will be not object"""
+    else isinstance(Rectangle == object)
+            """then Rectangle will be object"""
