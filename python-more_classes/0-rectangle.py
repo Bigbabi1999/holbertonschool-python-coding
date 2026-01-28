74f7c31a0cb0b12c6594a2176f57087337350178
Ali Moos <moosali446@gmail.com>
2026-01-28 09:37:40 +0400

    m

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -6,15 +6,18 @@ class Rectangle:
     """Represents a rectangle"""
 
     def __init__(self, width = 0, height = 0):
+
         self.width = width
         self.height = height
 
     @property
     def width(self):
+
         return self.__width
 
     @width.setter
     def widht(self, value):
+
         if not isinstance(value, int):
             raise TypeError("Width must be an integer")
         if value < 0:
@@ -23,10 +26,12 @@ class Rectangle:
 
     @property
     def height(self):
+
         return self.__height
 
     @height.setter
     def height(self, value):
+        
         if not isinstance(value, int):
             raise TypeError("Height must be an integer")
         if value < 0:
