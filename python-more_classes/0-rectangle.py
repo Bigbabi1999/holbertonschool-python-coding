5f85a755e770b94de9411c063d5d03982b291c28
Ali Moos <moosali446@gmail.com>
2026-02-03 18:14:18 +0400

    m

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-"""
-Defines a rectangle class
-"""
-
+"""Defines a rectangle class. """
 
 class Rectangle:
-    """
-    Represents a rectangle
-    """
+    """Represents a rectangle"""
 
     def __init__(self, width = 0, height = 0):
 
