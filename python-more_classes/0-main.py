2b3c7a9c9a15afa1e4c0840549b430d686820e62
Ali Moos <moosali446@gmail.com>
2026-02-06 18:36:17 +0400

    m

--- python-more_classes/0-main.py
+++ python-more_classes/0-main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+import inspect
 Rectangle = __import__('0-rectangle').Rectangle
+print(inspect.getsource(Rectangle))
+
 
 my_rectangle =Rectangle(2, 4)
 print(my_rectangle.__dict__)
--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """Represents a rectangle"""
 
-    def _init_(self, width=0, height=0):
+    def __init__(self, width=0, height=0):
         """Initialize rectangle with width and height"""
         self.width = width
         self.height = height
