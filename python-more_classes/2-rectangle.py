5e31940c4e3624c4b558110c481e3c88ecce4e5f
Ali Moos <moosali446@gmail.com>
2026-02-12 18:15:57 +0400

    m

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -31,3 +31,27 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """Return the ractangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+    
+    def __str__(self):
+        """Return the rectangle using # characters"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        
+        lines = []
+        for _ in range(self.__height):
+            lines.append("#" * self.__width)
+        return "\n".join(lines)
+    
+    def __repr__(self):
+        """Return a string representation to recreate the object"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
