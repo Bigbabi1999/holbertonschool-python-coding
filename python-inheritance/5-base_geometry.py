1c40caaf43459bc0365453e4a41015baa44c157e
Ali Moos <moosali446@gmail.com>
2026-02-28 19:08:28 +0400

    m

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Module that defines BaseGeometry"""
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Raises an exception becausearea is not implemented"""
+        raise Exception("area()is not implemented")
